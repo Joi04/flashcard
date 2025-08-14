10bdc613099ca2c34a1dd2680bb429cf4f06950c
Joi04 <braz.joice@escola.pr.gov.br>
2025-08-14 09:11:40 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -11,9 +11,9 @@ criaCartao(
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Gastronomia',
+    'Qual o prato mais conhecido na gatronomia brasileira?',
+    ''
 )
     criaCartao(
     'Lingua inglesa',
