598a5e434f23a5ca26bc1e7183b102eb5e5d6284
Joi04 <braz.joice@escola.pr.gov.br>
2025-08-21 08:34:48 -0300

    ,

--- perguntas.js
+++ perguntas.js
@@ -1,22 +1,26 @@
 criaCartao(
     'Coisas sobre cavalos',
     'Qual a é a raça de cavalo mais grande do mundo?',
+    ,
     'Shire'
 )
 
 criaCartao(
     'Carros esportivos',
     'Qual é o carro esportivo mais potente?',
+
     'Koenigsegg Gemera'
 )
 
 criaCartao(
     'Gastronomia',
     'Qual o prato mais conhecido na gatronomia brasileira?',
+
     'Feijoada'
 )
     criaCartao(
     'Montarias',
     'Quais são os tipos de montaria em cavalos?',
+
     'Cutiano, Sela Americana e Bareback'
 )
